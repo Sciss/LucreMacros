5824429aa4f8e8f3108da8c2491f8ff2b3cf42a1
Sciss <contact@sciss.de>
2014-01-10 13:35:07 +0100

    good, almost there

--- src/main/scala/de/sciss/lucre/macros/mkCompanion.scala
+++ src/main/scala/de/sciss/lucre/macros/mkCompanion.scala
@@ -15,7 +15,13 @@ object mkCompanionMacro {
     val outputs: List[Tree] = inputs match {
       case (cd @ ClassDef(_, cName, _, _)) :: tail =>
         val mod0: ModuleDef = tail match {
-          case (md @ ModuleDef(_, mName, mTemp)) :: Nil if cName.decoded == mName.decoded => md
+          case (md @ ModuleDef(_, mName, mTemp)) :: Nil if cName.decoded == mName.decoded =>
+            //            mTemp.children.foreach {
+            //              case DefDef(mods, name, tparams, vparamss, tpt, rhs) =>
+            //                println(s"Mods '$mods' name '$name', tparams '$tparams', vparamss '$vparamss', tpt '$tpt', rhs '$rhs'")
+            //              case _ =>
+            //            }
+            md
 
           case Nil =>
             val cMod = cd.mods
@@ -25,8 +31,12 @@ object mkCompanionMacro {
             if (cMod hasFlag Flag.LOCAL    ) mModF |= Flag.LOCAL
             val mMod = Modifiers(mModF, cMod.privateWithin, Nil)
 
-            // or should we have parents = List(AnyRef) and body = List(DefDef(???))
-            val mTemp = Template(parents = Nil, self = noSelfType, body = Nil)
+            val mkSuperSelect = Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR)
+            val superCall     = Apply(mkSuperSelect, Nil)
+            val constr        = DefDef(NoMods, nme.CONSTRUCTOR, Nil, List(Nil), TypeTree(),
+              Block(List(superCall), Literal(Constant())))
+
+            val mTemp = Template(parents = List(TypeTree(typeOf[AnyRef])), self = noSelfType, body = constr :: Nil)
             val mName = TermName(cName.decoded) // or encoded?
 
             ModuleDef(mMod, mName, mTemp)
@@ -36,7 +46,7 @@ object mkCompanionMacro {
 
         val Template(mTempParents, mTempSelf, mTempBody0) = mod0.impl
         val fooDef = DefDef(mods = NoMods, name = TermName("hasFoo"), tparams = Nil, vparamss = Nil,
-          tpt = EmptyTree /* TypeDef(NoMods, TypeName("Int"), Nil, EmptyTree) */, rhs = Literal(Constant(33)))
+          tpt = TypeTree(typeOf[Int]) /* EmptyTree */ /* TypeDef(NoMods, TypeName("Int"), Nil, EmptyTree) */, rhs = Literal(Constant(33)))
         val mTempBody1 = fooDef :: mTempBody0
         val mTemp1 = Template(mTempParents, mTempSelf, mTempBody1)
         val mod1 = ModuleDef(mod0.mods, mod0.name, mTemp1)
@@ -46,6 +56,6 @@ object mkCompanionMacro {
       case _ => c.abort(c.enclosingPosition, "Must annotate a class or trait")
     }
 
-    c.Expr[Any](Block(outputs: _*))
+    c.Expr[Any](Block(outputs, Literal(Constant(()))))
   }
 }
